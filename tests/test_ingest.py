d233d57add012d559d57e433fed97e570b22bcc5
kenn <kenn-feng@outlookc.com>
2026-06-18 18:17:24 -0400

    Add unit test for add_documents method with custom IDs in AtlasIngestor

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -15,3 +15,14 @@ def test_add_documents_generate_default_ids():
         metadatas=None,
         ids=["id_0", "id_1"],
     )
+
+def test_add_documents_with_custom_ids():
+    ingestor = make_mock_ingestor()
+    ingestor.collection.add = MagicMock()
+
+    ingestor.add_documents(["doc1", "doc2"], ids=["custom_id_1", "custom_id_2"])
+    ingestor.collection.add.assert_called_once_with(
+        documents=["doc1", "doc2"],
+        metadatas=None,
+        ids=["custom_id_1", "custom_id_2"],
+    )
